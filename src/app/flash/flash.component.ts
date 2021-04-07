f7ca96c1eeeb77b933bfbdacb6b62150949b15eb
AlexJanss <alex.janssen20@gmail.com>
2021-04-07 12:21:31 +0200

    flash card and model

--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { IFlash } from '../flash.model';
 
 @Component({
   selector: 'app-flash',
   templateUrl: './flash.component.html',
   styleUrls: ['./flash.component.css']
 })
-export class FlashComponent implements OnInit {
+export class FlashComponent {
+  @Input() flash: IFlash = {
+    id: 1,
+    question: 'React to Angular',
+    answer: 'No Reaction :)',
+    show: false,
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  };
 }
